16446c0f4819431fa9f3d1a0faa6a56408c2159d
Rafael Carvalho <falecomigo@rafaelcarvalho.tv>
2014-06-19 18:58:45 -0300

    fixing restart delayed job recipe

--- restart_delayed_job/recipes/default.rb
+++ restart_delayed_job/recipes/default.rb
@@ -2,6 +2,11 @@ Chef::Log.info("Restarting Delayed Job")
 
 node[:deploy].each do |application, deploy|
   execute "Restarting Delayed Job" do
-    command "sudo su -c 'RAILS_ENV=production #{deploy[:deploy_to]}/current/bin/delayed_job restart' #{deploy[:user]}"
+    command "sudo su #{deploy[:user]}"
+    command "cd #{deploy[:deploy_to]}/current"
+    command "RAILS_ENV=production #{deploy[:deploy_to]}/current/bin/delayed_job restart"
+    command "exit"
+    # command "sudo su #{deploy[:user]} -c 'RAILS_ENV=production #{deploy[:deploy_to]}/current/bin/delayed_job restart'"
   end
+
 end
