ed47024112d5ea6d0fbb8317877887a22a89502e
Rafael Carvalho <falecomigo@rafaelcarvalho.tv>
2014-06-20 11:48:22 -0300

    removing restart_delayed_job recipe, now using chef hooks

--- restart_delayed_job/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-Chef::Log.info("Restarting Delayed Job")
-
-node[:deploy].each do |application, deploy|
-  execute "Restarting Delayed Job" do
-    command "RAILS_ENV=production #{deploy[:deploy_to]}/current/bin/delayed_job restart"
-  end
-end
