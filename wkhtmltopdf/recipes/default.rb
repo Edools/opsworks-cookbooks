2e6112e0f30b9d3750b1f1e95f692db93654f08d
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2016-05-31 15:13:04 -0300

    split wkhtmltopdf commands

--- wkhtmltopdf/recipes/default.rb
+++ wkhtmltopdf/recipes/default.rb
@@ -1,8 +1,15 @@
 Chef::Log.info('Starting wkhtmltopdf check/install')
 
-execute "install wkhtmltopdf" do
+execute "downloading wkhtmltopdf" do
   command "wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb"
+end
+
+execute "install wkhtmltopdf" do
   command "sudo dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb"
+end
+
+execute "coping wkhtmltopdf" do
+  command "sudo cp /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf"
 end
 
 Chef::Log.info('Finishing wkhtmltopdf check/install')
