aafb30d190accf76c2d1c6011389f5383c35831c
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2016-05-31 14:13:02 -0300

    add wkhtmltopdf

---
+++ wkhtmltopdf/recipes/default.rb
@@ -0,0 +1,9 @@
+Chef::Log.info('Starting wkhtmltopdf check/install')
+
+execute "install wkhtmltopdf" do
+  command "wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb"
+  command "sudo dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb"
+  command "cp /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf"
+end
+
+Chef::Log.info('Finishing wkhtmltopdf check/install')
