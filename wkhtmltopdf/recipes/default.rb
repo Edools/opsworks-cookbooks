c8cbcab1fc5daa55c8fa7a5f03521d3696ac71f1
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2016-05-31 14:34:41 -0300

    fixing "cp" from wkhtmltopdf

--- wkhtmltopdf/recipes/default.rb
+++ wkhtmltopdf/recipes/default.rb
@@ -3,7 +3,7 @@ Chef::Log.info('Starting wkhtmltopdf check/install')
 execute "install wkhtmltopdf" do
   command "wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb"
   command "sudo dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb"
-  command "cp /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf"
+  command "sudo cp /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf"
 end
 
 Chef::Log.info('Finishing wkhtmltopdf check/install')
