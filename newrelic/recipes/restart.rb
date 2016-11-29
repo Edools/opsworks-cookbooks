2b24c4afe6325d694ee7d5a522514cca2f1bfa35
William Belchior <williamsbelchior@gmail.com>
2016-11-29 10:12:17 -0200

    fix start/stop newrelic conditions

--- newrelic/recipes/restart.rb
+++ newrelic/recipes/restart.rb
@@ -1,14 +1,18 @@
 Chef::Log.info("[Start] Restart newrelic")
 
 execute "stop newrelic" do
+	only_if('sudo initctl status newrelic-infra | grep running')
+	Chef::Log.info("TEST STOP NEWRELIC #1")
 	only_if('sudo initctl status newrelic-infra | grep running -c')
-
+	Chef::Log.info("TEST STOP NEWRELIC #2")
 	command('sudo initctl stop newrelic-infra')
 end
 
 execute "start newrelic" do
+	not_if('sudo initctl status newrelic-infra | grep running')
+	Chef::Log.info("TEST START NEWRELIC #1")
 	not_if('sudo initctl status newrelic-infra | grep running -c')
-
+	Chef::Log.info("TEST START NEWRELIC #2")
 	command('sudo initctl start newrelic-infra')
 end
 
