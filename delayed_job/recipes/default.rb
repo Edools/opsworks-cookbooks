5ad01f893711fb87177bf394f43c258946181252
williamsbelchior <williamsbelchior@gmail.com>
2017-01-09 20:14:10 -0200

    delayed job rake jobs work

--- delayed_job/recipes/default.rb
+++ delayed_job/recipes/default.rb
@@ -12,17 +12,9 @@ node[:deploy].each do |application, deploy|
     Chef::Log.info("Restarting delayed_job ...")
 
     code <<-EOH
-      touch start.sh
-    EOH
-    
-    file 'start.sh' do
-      content '
-        #! /bin/sh
-        RAILS_ENV=production bundle exec rake jobs:work
-      '
-    end
-
-    code <<-EOH
+      rm -f start.sh
+      echo "#! /bin/sh" >> start.sh
+      echo "RAILS_ENV=production bundle exec rake jobs:work" >> start.sh
       kill -9 $(ps aux | grep jobs:work | grep -v grep | awk '{print $2}')
       chmod +x start.sh
       nohup ./start.sh 0<&- &> my.admin.log.file &
