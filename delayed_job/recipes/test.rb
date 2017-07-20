4b6f780abf0b28fbcf3f27a50309e67bc4d1bbc4
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2017-07-20 14:04:35 -0300

    Update test.rb

--- delayed_job/recipes/test.rb
+++ delayed_job/recipes/test.rb
@@ -1,9 +1,25 @@
-Chef::Log.info("[Start] Testing recipe")
+Chef::Log.info("[Restart] Delayed Job ...")
 
-node[:deploy].each do |application, deploy_config|
-  application_deploy = node[:deploy][application]
-  Chef::Log.info("application_deploy path: #{application_deploy[:deploy_to]}")
-  Chef::Log.info("deploy_config path: #{application_deploy[:deploy_to]}")
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  bash "restart-delayed_job-#{application}" do
+    layers = node[:opsworks][:instance][:layers]
+
+    cwd "/srv/www/core/current"
+    user 'deploy'
+
+    Chef::Log.info("Restarting delayed_job ...")
+
+    code <<-EOH
+      rm -f start.sh
+      echo "#! /bin/sh" >> start.sh
+      echo "RAILS_ENV=production bundle exec rake jobs:work" >> start.sh
+      kill -9 $(ps aux | grep jobs:work | grep -v grep | awk '{print $2}')
+      chmod +x start.sh
+      nohup ./start.sh 0<&- &> my.admin.log.file &
+    EOH
+  end
 end
 
-Chef::Log.info("[End] Tested recipe")
+Chef::Log.info("[End] Restarting Delayed Job")
