4f576144e1203acc951b208e13d6e448c02c4109
williamsbelchior <williamsbelchior@gmail.com>
2017-08-31 15:53:40 -0300

    change zeus setup to bash code

--- zeus/recipes/setup.rb
+++ zeus/recipes/setup.rb
@@ -1,16 +1,18 @@
 Chef::Log.info("[Start] Setup zeus")
 
-execute "install zues dependencies" do
-  command("sudo apt-get update -y ")
-  command("sudo apt-get install nodejs -y")
-  command("sudo apt-get install npm -y")
-  command("sudo apt-get install python -y")
-  command("sudo npm install n --global")
-  command("sudo n 5.6.0")
-  command("sudo npm install node-gyp --global")
-  command("sudo npm install pm2 --global")
-  command("sudo apt-get install libcap2-bin")
-  command("sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\ ")
+bash "install zues dependencies" do
+  code <<-EOH
+    sudo apt-get update -y 
+    sudo apt-get install nodejs -y
+    sudo apt-get install npm -y
+    sudo apt-get install python -y
+    sudo npm install n --global
+    sudo n 5.6.0
+    sudo npm install node-gyp --global
+    sudo npm install pm2 --global
+    sudo apt-get install libcap2-bin
+    sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\ 
+  EOH
 end
 
 Chef::Log.info("[End] Setup zeus")
