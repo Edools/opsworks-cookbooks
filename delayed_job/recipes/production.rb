a53cf2ac2e08a8b237ed76369bf21cfa22501940
William Belchior <williamsbelchior@gmail.com>
2018-05-03 12:09:56 -0300

    Update production.rb

--- delayed_job/recipes/production.rb
+++ delayed_job/recipes/production.rb
@@ -14,13 +14,14 @@ node[:deploy].each do |application, deploy|
     code <<-EOH
       rm -f start.sh
       echo "#! /bin/sh" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i payment --queue=payment start" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i hermes --queue=hermes start" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i default --queue=default start" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i report --queue=report start" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i lesson_progress_1 --queue=lesson_progress start" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i lesson_progress_2 --queue=lesson_progress start" >> start.sh
-      echo "RAILS_ENV=production bin/delayed_job -i bulk_invitation --queue=bulk_invitation start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i payment --queue=payment start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i hermes --queue=hermes start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i hermes2 --queue=hermes start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i default --queue=default start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i default2 --queue=default start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i report --queue=report start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i bulk_invitation --queue=bulk_invitation start" >> start.sh
+      echo "RAILS_ENV=staging bin/delayed_job -i searchkick --queue=searchkick start" >> start.sh
       kill -9 $(ps aux | grep delayed_job | grep -v grep | awk '{print $2}')
       chmod +x start.sh
       nohup ./start.sh 0<&- &> my.admin.log.file &
