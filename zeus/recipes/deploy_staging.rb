5e1c6676d50ead76ca42fcdf564038248ca8d287
Jorge Roberto Tomaz Junior <jorgerobertodev@gmail.com>
2018-08-08 14:05:04 -0300

    change url to run specific job

--- zeus/recipes/deploy_staging.rb
+++ zeus/recipes/deploy_staging.rb
@@ -1,7 +1,8 @@
 Chef::Log.info("[Start] Deploy zeus staging")
 
 http_request 'deploy staging zeus circle' do
-  url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28"
+  # url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28"
+  url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28&build_parameters[CIRCLE_JOB]=staging_deploy"
   action :post
 end
 
