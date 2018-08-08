ada4a67512abbb77cfa5882908ae4beb2240ebc4
Jorge Roberto Tomaz Junior <jorgerobertodev@gmail.com>
2018-08-08 17:37:40 -0300

    trying delay to execute job after setup machine

--- zeus/recipes/deploy_staging.rb
+++ zeus/recipes/deploy_staging.rb
@@ -1,5 +1,7 @@
 Chef::Log.info("[Start] Deploy zeus staging")
 
+sleep(2)
+
 http_request 'deploy staging zeus circle' do
   # url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28"
   url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28&build_parameters[CIRCLE_JOB]=staging_deploy"
