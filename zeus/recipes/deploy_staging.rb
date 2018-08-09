6ca8567c2adb3618e8dcfcfe93fc9bc8e20f1cf6
William Belchior <williamsbelchior@gmail.com>
2018-08-09 17:55:52 -0300

    Update deploy_staging.rb

--- zeus/recipes/deploy_staging.rb
+++ zeus/recipes/deploy_staging.rb
@@ -1,11 +1,17 @@
 Chef::Log.info("[Start] Deploy zeus staging")
 
-sleep(2)
-
 http_request 'deploy staging zeus circle' do
-  # url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28"
-  url "https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28&build_parameters[CIRCLE_JOB]=staging_deploy"
-  action :post
+  action(:post)
+  
+  url("https://circleci.com/api/v1.1/project/github/Edools/zeus/tree/dev?circle-token=f22ba03ebad89e84d1b56297557bb8bb499f9d28")
+  
+  message({ 
+    'build_parameters' => { 'CIRCLE_JOB' => 'staging_deploy' } 
+  }.to_json)
+  
+  headers({
+    'Content-Type' => 'application/data'
+  })
 end
 
 Chef::Log.info("[End] Deploy zeus staging")
