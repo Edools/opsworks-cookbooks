5a8fa30c19fb333e6a4c2f9e25041b769f9cc568
Rafael Carvalho <rafael@edools.com>
2019-05-10 12:47:01 -0300

    adding recipe to install brotli

---
+++ brotli/recipes/default.rb
@@ -0,0 +1,36 @@
+Chef::Log.info('Installing brotli')
+
+execute "installing unzip" do
+  command "sudo apt-get install unzip"
+end
+
+execute "installing cmake" do
+  command "sudo apt-get -y install cmake"
+end
+
+execute "downloading brotli" do
+  command "wget https://github.com/google/brotli/archive/master.zip"
+end
+
+execute "unziping brotli" do
+  command "unzip master.zip"
+end
+
+execute "entering brotli directory" do
+  command "cd brotli-master"
+end
+
+execute "preparing to compile brotli" do
+  command "./configure-cmake"
+end
+
+execute "compilling brotli" do
+  command "make"
+end
+
+execute "installing brotli" do
+  command "sudo make install"
+end
+
+
+Chef::Log.info('Finishing Install')
