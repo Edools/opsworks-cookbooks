f5d171d3c8b40698c3fc3d81be6d10bbd9247d94
Rafael Carvalho <rafael@edools.com>
2020-04-21 14:52:30 -0300

    finishing botpress setup

--- vision/recipes/botpress_setup.rb
+++ vision/recipes/botpress_setup.rb
@@ -1,26 +1,5 @@
 Chef::Log.info("[Start] Setup Botpress")
 
-# setup_sh_file = '/setup_botpress.sh'
-# 
-# file setup_sh_file do
-#   content '
-#     # setup.sh
-#     #! /bin/bash
-#     sudo apt-get install --yes --force-yes unzip
-# 
-# 
-#     wget https://s3.amazonaws.com/botpress-binaries/botpress-v12_8_2-linux-x64.zip
-#     unzip botpress-v12_8_2-linux-x64.zip
-# 
-#     # Launch the app
-#     ./bp
-#   '
-# end
-# 
-# bash 'setup botpress' do
-#   code 'source /setup.sh'
-# end
-
 botpress_file_name = "botpress-#{node[:botpress][:version]}-#{node[:botpress][:platform]}.zip"
 botpress_url = "https://s3.amazonaws.com/botpress-binaries/#{botpress_file_name}"
 
@@ -53,6 +32,24 @@ execute "download botpress binary to #{node[:botpress][:version]} version" do
   cwd "#{botpress_current_dir}"
   
   not_if do
+    ::File.exists?("#{botpress_current_dir}/#{botpress_file_name}")
+  end
+end
+
+execute "unzip #{botpress_file_name}" do
+  command "unzip #{botpress_file_name}"
+  cwd "#{botpress_current_dir}"
+  
+  not_if do
+    ::File.exists?("#{botpress_current_dir}/bp")
+  end
+end
+
+execute "remove zip file #{botpress_file_name}" do
+  command "rm -rf #{botpress_file_name}"
+  cwd "#{botpress_current_dir}"
+  
+  only_if do
     ::File.exists?("#{botpress_current_dir}/#{botpress_file_name}")
   end
 end
