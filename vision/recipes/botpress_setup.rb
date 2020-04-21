5675fb56f940fbbfa4d230eba041d6561c022c58
Rafael Carvalho <rafael@edools.com>
2020-04-21 14:12:13 -0300

    creating botpress directory

---
+++ vision/recipes/botpress_setup.rb
@@ -0,0 +1,40 @@
+Chef::Log.info("[Start] Setup Botpress")
+
+# setup_sh_file = '/setup_botpress.sh'
+# 
+# file setup_sh_file do
+#   content '
+#     # setup.sh
+#     #! /bin/bash
+#     sudo apt-get install --yes --force-yes unzip
+# 
+# 
+#     wget https://s3.amazonaws.com/botpress-binaries/botpress-v12_8_2-linux-x64.zip
+#     unzip botpress-v12_8_2-linux-x64.zip
+# 
+#     # Launch the app
+#     ./bp
+#   '
+# end
+# 
+# bash 'setup botpress' do
+#   code 'source /setup.sh'
+# end
+
+execute "create botpress source dir" do
+  command("mkdir #{node[:botpress][:souce_dir]}")
+  
+  not_if do
+    ::File.directory?("#{node[:botpress][:souce_dir]}")
+  end
+end
+
+execute "create botpress source dir to #{node[:botpress][:version]} version" do
+  command("mkdir #{node[:botpress][:souce_dir][:version]}")
+  
+  not_if do
+    ::File.directory?("#{node[:botpress][:souce_dir][:version]}")
+  end
+end
+
+Chef::Log.info("[End] Start BotPress ")
--- vision/recipes/ngnix_config.rb
+++ vision/recipes/ngnix_config.rb
@@ -10,63 +10,10 @@ execute "move nginx configuration file to tmp" do
   end
 end
 
-# file nginx_conf_file do
-#   content '	user www-data;
-#             worker_processes  2;
-#             pid        /run/nginx.pid;
-# 
-#             events {
-#               worker_connections  65536;
-#               multi_accept        on;
-#               use                 epoll;
-#             }
-# 
-#             http {
-#               include       /etc/nginx/mime.types;
-#               default_type  application/octet-stream;
-# 
-#               # Disable sending the server identification
-#               server_tokens off;
-# 
-#               # Prevent displaying Botpress in an iframe (clickjacking protection)
-#               add_header X-Frame-Options SAMEORIGIN;
-# 
-#               # Prevent browsers from detecting the mimetype if not sent by the server.
-#               add_header X-Content-Type-Options nosniff;
-# 
-#               # Force enable the XSS filter for the website, in case it was disabled manually
-#               add_header X-XSS-Protection "1; mode=block";
-# 
-#               # Configure the cache for static assets
-#               proxy_cache_path /srv/nginx_cache levels=1:2 keys_zone=my_cache:10m max_size=10g inactive=60m use_temp_path=off;
-# 
-#               # Set the max file size for uploads (make sure it is larger than the configured media size in botpress.config.json)
-#               client_max_body_size 10M;              
-# 
-#               access_log	/var/log/nginx/access.log;
-#               access_log off;
-#               error_log  /var/log/nginx/error.log;
-# 
-#               # Redirect requests to the server
-#               server {
-#                 listen 80 default;
-#                 server_name  localhost;
-# 
-#                 # All other requests should be directed to the server
-#                 location / {
-#                   proxy_pass http://localhost:3000;
-#                 }
-#               }
-# 
-#               # include /etc/nginx/conf.d/*.conf;
-#               # include /etc/nginx/sites-enabled/*;
-#             }'
-# end
-
 Chef::Log.info("Creating Ngnix config file")
 template "/etc/nginx/nginx.conf" do
   source "nginx.conf.erb"
-  mode "0755"
+  mode "0644"
   owner "root"
   group "root"
 end
