c38a564c768fd3e6c76e244273016fe43870f79a
Rafael Carvalho <rafael@edools.com>
2020-05-17 10:42:26 -0300

    fix

--- heimdall/cookbooks/heimdall/recipes/whenever.rb
+++ heimdall/cookbooks/heimdall/recipes/whenever.rb
@@ -1,20 +1,22 @@
 Chef::Log.info("[Start] Updating Cron by Whenever")
 
 app = search("aws_opsworks_app").first
+deploy_to = "#{node[:deploy][:to]}/#{app[:shortname]}"
+rails_env = app[:environment]['RAILS_ENV']
 
 node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
+  # deploy = node[:deploy][application]
 
   bash "update-crontab" do
     # layers = node[:opsworks][:instance][:layers]
 
-    cwd "#{deploy[:deploy_to]}/current"
+    cwd "#{deploy_to}/current"
     user 'deploy'
 
     Chef::Log.info("Updating cron ...")
 
-    code "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{app[:shortname]}"
-    only_if "cd #{deploy[:deploy_to]}/current && bundle show whenever"
+    code "bundle exec whenever --set environment=#{rails_env} --update-crontab #{app[:shortname]}"
+    only_if "cd #{deploy_to}/current && bundle show whenever"
   end
 end
 
